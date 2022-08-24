26edc89e17e926246f7c6643737c6aecafcedb77
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-08-24 12:59:58 +0200

    Create LoginPage and add link to RegisterPage :rocket:

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,64 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+      <Grid
+        container
+        spacing={ 0 }
+        direction='column'
+        alignItems='center'
+        justifyContent='center'
+        sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+      >
+
+        <Grid item
+          className='box-shadow'
+          xs={ 3 }
+          sx={{ backgroundColor: 'white', padding: 3, borderRadious: 2 }}
+        >
+
+            <Typography varient='h5' sx={{ mb: 1 }}>Login</Typography>
+
+            <form>
+                <Grid container>
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                          label='Email'
+                          type='email'
+                          placeholder='email@example.com'
+                          fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                          label='Password'
+                          type='password'
+                          placeholder='password'
+                          fullWidth
+                        />
+                    </Grid>
+
+                    <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={ 12 }>
+                            <Button variant='contained' fullWidth>
+                                Login
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction='row' justifyContent='end'>
+                        <Link component={ RouterLink } color='inherit' to='/auth/register'>
+                            Create an account 🆕
+                        </Link>
+                    </Grid>
+
+                </Grid>
+            </form>
+
+        </Grid>
+
+      </Grid>
   )
 }
